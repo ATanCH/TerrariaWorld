b749459b55be0cada8d83cb3c7405adfabe17f0c
Jeremy Chan <chanjem@gmail.com>
2022-04-29 17:39:33 +0800

    Update batch

--- start-server.bat
+++ start-server.bat
@@ -1,8 +1,17 @@
+git pull || goto :gitError
+
+goto :start
+
 :start
 TerrariaServer.exe -config serverconfig.txt
 
 git add Worlds
-git commit -m "Update world"
+git commit -m "Update world - $(date)"
 git push
 
-# pause
+pause
+exit 0
+
+:gitError
+echo "Error on pulling latest changes"
+pause
